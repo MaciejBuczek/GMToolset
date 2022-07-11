1fc996b64de341e86e765990c1eca466a64703af
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2022-07-11 18:38:03 +0200

    basic ef config

---
+++ GMToolset.Data/UrlConnectionStringParser.cs
@@ -0,0 +1,19 @@
+﻿namespace GMToolset.Data
+{
+    public class UrlConnectionStringParser
+    {
+		public string GetConnectionString(string dbUrl)
+		{
+			bool isUrl = Uri.TryCreate(dbUrl, UriKind.Absolute, out Uri uri);
+			if (isUrl && uri != null)
+			{
+				var connectionUrl = $"host={uri.Host};username={uri.UserInfo.Split(':')[0]};password={uri.UserInfo.Split(':')[1]};database={uri.LocalPath.Substring(1)};pooling=true;";
+				return connectionUrl;
+			}
+			else
+			{
+				return string.Empty;
+			}
+		}
+	}
+}
