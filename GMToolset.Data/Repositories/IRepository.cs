ec08f05ec99eeac9514fa328eace03cf2bbee1c8
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2022-07-17 20:48:56 +0200

    Repository interfaces added

---
+++ GMToolset.Data/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace GMToolset.Data.Repositories
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetById();
+        
+        IEnumerable<T> GetAll();
+        
+        IEnumerable<T> GetByQuery(Func<T> query);
+        
+        void Add(T entity);
+        
+        void Update(T entity);
+        
+        void Delete(T entity);
+    }
+}
---
+++ GMToolset.Data/Repositories/IRepositoryAsync.cs
@@ -0,0 +1,17 @@
+﻿namespace GMToolset.Data.Repositories
+{
+    public interface IRepositoryAsync<T>
+    {
+        Task<IEnumerable<T>> GetById();
+
+        Task<IEnumerable<T>> GetAll();
+
+        Task<IEnumerable<T>> GetByQuery(Func<T> query);
+
+        Task Add(T entity);
+
+        Task Update(T entity);
+
+        Task Delete(T entity);
+    }
+}
