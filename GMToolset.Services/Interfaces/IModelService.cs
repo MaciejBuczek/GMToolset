7a3f0974684d227d095027bd400e0c8611e85505
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2022-07-17 21:24:42 +0200

    Service layer model provider classes set up

---
+++ GMToolset.Services/Interfaces/IModelService.cs
@@ -0,0 +1,15 @@
+﻿namespace GMToolset.Services.Interfaces
+{
+    public interface IModelService<T>
+    {
+        T GetById(Guid id);
+
+        IEnumerable<T> GetAll();
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(Guid id);
+    }
+}
---
+++ GMToolset.Services/Services/Model Services/CharacterSheetService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using GMToolset.Data.Entities.Warhammer4;
+using GMToolset.Data.Repositories.Interfaces;
+using GMToolset.Services.Interfaces;
+using Entities = GMToolset.Data.Entities.Warhammer4;
+
+namespace GMToolset.Services.Services.Model_Services
+{
+    public class CharacterSheetService : ModelServiceBase<Entities.CharacterSheet>, IModelService<CharacterSheet>
+    {
+        public CharacterSheetService(IRepository<CharacterSheet> repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+
+        public void Add(CharacterSheet entity)
+        {
+            _repository.Add(_mapper.Map<Entities.CharacterSheet>(entity));
+        }
+
+        public void Delete(Guid id)
+        {
+            _repository.Delete(id);
+        }
+
+        public IEnumerable<CharacterSheet> GetAll()
+        {
+            return _mapper.Map<IEnumerable<CharacterSheet>>(_repository.GetAll());
+        }
+
+        public CharacterSheet GetById(Guid id)
+        {
+            return _mapper.Map<CharacterSheet>(_repository.GetById(id));
+        }
+
+        public void Update(CharacterSheet entity)
+        {
+            _repository.Update(_mapper.Map<Entities.CharacterSheet>(entity);
+        }
+    }
+}
