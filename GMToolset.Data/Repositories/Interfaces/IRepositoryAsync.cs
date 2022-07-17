4ea12d0492da7cff13a4145aa653c90d854be785
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2022-07-17 21:13:44 +0200

    Repository interfaces changes

---
+++ GMToolset.Data/Repositories/Interfaces/IRepositoryAsync.cs
@@ -0,0 +1,15 @@
+﻿namespace GMToolset.Data.Repositories.Interfaces
+{
+    public interface IRepositoryAsync<T>
+    {
+        Task<T> GetById(Guid id);
+
+        Task<IEnumerable<T>> GetAll();
+
+        Task Add(T entity);
+
+        Task Update(T entity);
+
+        Task Delete(Guid id);
+    }
+}
