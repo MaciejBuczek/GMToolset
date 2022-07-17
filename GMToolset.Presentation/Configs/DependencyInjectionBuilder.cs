b0f8e4b0a71f8d952a2258051699cb3975eae753
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2022-07-17 18:29:22 +0200

    Current dependency injection moved to seperate class

---
+++ GMToolset.Presentation/Configs/DependencyInjectionBuilder.cs
@@ -0,0 +1,30 @@
+﻿using GMToolset.Data;
+using GMToolset.Services.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace GMToolset.Presentation.Configs
+{
+    public class DependencyInjectionBuilder
+    {
+        public void AddDependencies(WebApplicationBuilder builder)
+        {
+            //Database context setup
+            var connectionString = builder.Environment.IsDevelopment() ?
+                builder.Configuration.GetConnectionString("Local") :
+                new UrlConnectionStringParser().GetConnectionString(Environment.GetEnvironmentVariable("DATABASE_URL"));
+            builder.Services.AddDbContext<AppDbContext>(
+                    o => o.UseNpgsql(connectionString)
+                );
+
+            //Email sender setup
+            builder.Services.AddTransient<IEmailSender, EmailSender>();
+            builder.Services.Configure<AuthMessageSenderOptions>(o =>
+            o.SendGridKey = Environment.GetEnvironmentVariable("SENDGRIDKEY"));
+
+            builder.Services.Configure<DataProtectionTokenProviderOptions>(o =>
+                   o.TokenLifespan = TimeSpan.FromHours(2));
+        }
+    }
+}
--- GMToolset.Presentation/Program.cs
+++ GMToolset.Presentation/Program.cs
@@ -3,25 +3,16 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using GMToolset.Services.Services;
+using GMToolset.Presentation.Configs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Environment.IsDevelopment() ?
-    builder.Configuration.GetConnectionString("Local") :
-    new UrlConnectionStringParser().GetConnectionString(Environment.GetEnvironmentVariable("DATABASE_URL"));
-builder.Services.AddDbContext<AppDbContext>(
-        o => o.UseNpgsql(connectionString)
-    );
-
+// Identity setup
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<AppDbContext>();
 
-builder.Services.AddTransient<IEmailSender, EmailSender>();
-builder.Services.Configure<AuthMessageSenderOptions>(o =>
-o.SendGridKey = Environment.GetEnvironmentVariable("SENDGRIDKEY"));
-
-builder.Services.Configure<DataProtectionTokenProviderOptions>(o =>
-       o.TokenLifespan = TimeSpan.FromHours(2));
+//Dependency Injection setup
+new DependencyInjectionBuilder().AddDependencies(builder);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
