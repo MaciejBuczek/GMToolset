dba849740f7b352872c4dab245a234331f4ae0c4
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2022-07-17 21:14:07 +0200

    Character Sheet Repository class added

---
+++ GMToolset.Data/Repositories/CharacterSheetRepository.cs
@@ -0,0 +1,46 @@
+﻿using GMToolset.Data.Entities.Warhammer4;
+using GMToolset.Data.Repositories.Interfaces;
+
+namespace GMToolset.Data.Repositories
+{
+    public class CharacterSheetRepository : RepositoryBase, IRepository<CharacterSheet>
+    {
+        public CharacterSheetRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+
+        public void Add(CharacterSheet entity)
+        {
+            _appDbContext.Add(entity);
+        }
+
+        public void Delete(Guid id)
+        {
+            var entity = _appDbContext.CharacterSheets.Find(id);
+            if(entity != null)
+            {
+                _appDbContext.Remove(entity);
+                _appDbContext.SaveChanges();
+            }
+        }
+
+        public IEnumerable<CharacterSheet> GetAll()
+        {
+            return _appDbContext.CharacterSheets.AsEnumerable();
+        }
+
+        public CharacterSheet GetById(Guid id)
+        {
+            return _appDbContext.CharacterSheets.Find(id);
+        }
+
+        public void Update(CharacterSheet entity)
+        {
+            if(_appDbContext.CharacterSheets.Find(entity.Id) != null)
+            {
+                _appDbContext.Update(entity);
+                _appDbContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ GMToolset.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿namespace GMToolset.Data.Repositories
+{
+    public class RepositoryBase
+    {
+        protected readonly AppDbContext _appDbContext;
+
+        public RepositoryBase(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+    }
+}
