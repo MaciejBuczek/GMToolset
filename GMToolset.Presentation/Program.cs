5e68409c948697d02b7d46b72eeb4364a9308f09
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2022-11-04 19:33:30 +0100

    Changed culture tag from en to en-us

--- GMToolset.Presentation/Program.cs
+++ GMToolset.Presentation/Program.cs
@@ -32,7 +32,7 @@ builder.Services.Configure<RequestLocalizationOptions>(options =>
     var supportedCultures = new List<CultureInfo>
     {
         new CultureInfo("pl"),
-        new CultureInfo("en")
+        new CultureInfo("en-us")
     };
     options.DefaultRequestCulture = new RequestCulture("pl");
     options.SupportedCultures = supportedCultures;
