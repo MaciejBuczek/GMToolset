5a09c98cb4bbb34541d28c0b25a9ae7a6b1142e3
Szymon Buch <szymonbuch1@gmail.com>
2022-11-05 12:52:12 +0100

    Added repositories for Participant and QuickSession

---
+++ GMToolset.Data/Repositories/QuickSessionRepository.cs
@@ -0,0 +1,45 @@
+using GMToolset.Data.Entities.QuickBattleManager;
+using GMToolset.Data.Repositories.Interfaces;
+
+namespace GMToolset.Data.Repositories
+{
+    public class QuickSessionRepository : RepositoryBase, IRepository<QuickSession>
+    {
+        public QuickSessionRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+        public void Add(QuickSession entity)
+        {
+            _appDbContext.Add(entity);
+        }
+
+        public void Delete(Guid id)
+        {
+            var entity = _appDbContext.QuickSessions.Find(id);
+            if (entity != null)
+            {
+                _appDbContext.Remove(entity);
+                _appDbContext.SaveChanges();
+            }
+        }
+
+        public IEnumerable<QuickSession> GetAll()
+        {
+            return _appDbContext.QuickSessions.AsEnumerable();
+        }
+
+        public QuickSession GetById(Guid id)
+        {
+            return _appDbContext.QuickSessions.Find(id);
+        }
+
+        public void Update(QuickSession entity)
+        {
+            if (_appDbContext.QuickSessions.Find(entity.Id) != null)
+            {
+                _appDbContext.Update(entity);
+                _appDbContext.SaveChanges();
+            }
+        }
+    }
+}
