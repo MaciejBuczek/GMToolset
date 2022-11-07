f7ccbde860f0f106570f60d22da9f03d57df5e3c
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2022-11-07 12:58:39 +0100

    Base character page created: Base info, apperance, characteristics, fate and resilience

---
+++ GMToolset.Presentation/Controllers/CharacterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GMToolset.Presentation.Controllers
+{
+    public class CharacterController : Controller
+    {
+        public IActionResult CharacterSheet()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
