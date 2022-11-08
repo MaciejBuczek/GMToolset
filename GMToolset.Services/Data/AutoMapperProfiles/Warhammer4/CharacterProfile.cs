b8dffc2aab5323c7290f83386e9a440e7c5e6644
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2022-11-08 19:11:15 +0100

    Service layer configured to use new entities

---
+++ GMToolset.Services/Data/AutoMapperProfiles/Warhammer4/CharacterProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using GMToolset.Services.Models.Warhammer4.Character;
+
+namespace GMToolset.Services.Data.AutoMapperProfiles.Warhammer4
+{
+    public class CharacterProfile : Profile
+    {
+        public CharacterProfile()
+        {
+            CreateMap<GMToolset.Data.Entities.Warhammer4.Character.CharacterSheet, CharacterSheet>().ReverseMap();
+
+            CreateMap<GMToolset.Data.Entities.Warhammer4.Character.Skill, Skill>().ReverseMap();
+            CreateMap<GMToolset.Data.Entities.Warhammer4.Character.SkillType, SkillType>().ReverseMap();
+
+            CreateMap<GMToolset.Data.Entities.Warhammer4.Character.Characteristic, Characteristic>().ReverseMap();
+
+            CreateMap<GMToolset.Data.Entities.Warhammer4.Character.CharacterSkills, CharacterSkills>().ReverseMap();
+            CreateMap<GMToolset.Data.Entities.Warhammer4.Character.CharacterCharacteristics, Characteristics>().ReverseMap();
+        }
+    }
+}
---
+++ GMToolset.Services/Services/Model Services/Warhammer4/CharacteristicService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using GMToolset.Data.Repositories.Interfaces;
+using GMToolset.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using _Entities = GMToolset.Data.Entities.Warhammer4.Character;
+using _Models = GMToolset.Services.Models.Warhammer4.Character;
+
+namespace GMToolset.Services.Services.Model_Services.Warhammer4
+{
+    public class CharacteristicService : ModelServiceBase<_Entities.Characteristic>, IModelService<_Models.Characteristic>
+    {
+        public CharacteristicService(IRepository<_Entities.Characteristic> repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+
+        public void Add(_Models.Characteristic entity)
+        {
+            _repository.Add(_mapper.Map<_Entities.Characteristic>(entity));
+        }
+
+        public void Delete(Guid id)
+        {
+            _repository.Delete(id);
+        }
+
+        public IEnumerable<_Models.Characteristic> GetAll()
+        {
+            return _mapper.Map<IEnumerable<_Models.Characteristic>>(_repository.GetAll());
+        }
+
+        public _Models.Characteristic GetById(Guid id)
+        {
+            return _mapper.Map<_Models.Characteristic>(_repository.GetById(id));
+        }
+
+        public void Update(_Models.Characteristic entity)
+        {
+            _repository.Update(_mapper.Map<_Entities.Characteristic>(entity));
+        }
+    }
+}
