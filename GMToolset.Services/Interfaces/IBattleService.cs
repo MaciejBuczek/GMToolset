ed9b06c52814aeea421d31aeb06972340d135f3c
Szymon Buch <szymonbuch1@gmail.com>
2022-11-17 17:51:35 +0100

    Created QuickSessionManager and Service

---
+++ GMToolset.Services/Interfaces/IBattleService.cs
@@ -0,0 +1,17 @@
+﻿using GMToolset.Services.Models.QuickBattleManager;
+
+namespace GMToolset.Services.Interfaces
+{
+    public interface IBattleService
+    {
+        void EnqueueParticipants(IEnumerable<Participant> participants);
+
+        List<Participant> GetParticipants();
+
+        void AddParticipant(Participant participant);
+
+        void RemoveParticipant(Guid participantId);
+
+        void UpdateParticipant(Participant participant);
+    }
+}
