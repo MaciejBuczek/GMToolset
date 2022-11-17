4d411e11eed6ab7ad608df604c34f581b4a9f726
Szymon Buch <szymonbuch1@gmail.com>
2022-11-17 18:43:17 +0100

    Added BattleService implementation

---
+++ GMToolset.Services/Helpers/Extensions/ParticipantExtensions.cs
@@ -0,0 +1,22 @@
+﻿using GMToolset.Services.Models.QuickBattleManager;
+
+namespace GMToolset.Services.Helpers.Extensions
+{
+    public static class ParticipantExtensions
+    {
+        public static void Update(this Participant current, Participant other)
+        {
+            if (other == null)
+                throw new ArgumentNullException(nameof(other));
+
+            current.MaxHp = other.MaxHp;
+            current.CurrentHp = other.CurrentHp;
+            current.MaxMp = other.MaxMp;
+            current.CurrentMp = other.CurrentMp;
+            current.MaxSt = other.MaxSt;
+            current.CurrentSt = other.CurrentSt;
+            current.Initiative = other.Initiative;
+            current.DamagePerRound = other.DamagePerRound;
+        }
+    }
+}
