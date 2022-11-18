1f09472502f46667b33177a2f7ef8969eea759e5
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2022-11-18 12:11:23 +0100

    Controller actions for skill types created

---
+++ GMToolset.Presentation/Controllers/SkillTypesController.cs
@@ -0,0 +1,95 @@
+﻿using GMToolset.Presentation.ViewModels.Warhammer4.CRUD;
+using GMToolset.Services.Interfaces;
+using GMToolset.Services.Models.Warhammer4;
+using GMToolset.Services.Models.Warhammer4.Character.Skills;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GMToolset.Presentation.Controllers
+{
+    public class SkillTypesController : Controller
+    {
+        private readonly IModelService<SkillType> _skillTypeService;
+
+        public SkillTypesController(IModelService<SkillType> skillTypeService)
+        {
+            _skillTypeService = skillTypeService;
+        }
+
+        [HttpGet]
+        public IActionResult Manage()
+        {
+            var vm = new TranslationBasedManageVM<SkillType>
+            {
+                ContentEng = String.Empty,
+                ContentPl = String.Empty,
+                Entries = _skillTypeService.GetAll()
+            };
+            ModelState.Clear();
+            return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult ManageResult(TranslationBasedManageVM<SkillType> vm)
+        {
+            return View(nameof(Manage), vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(TranslationBasedManageVM<SkillType> vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var characteristic = new SkillType
+                {
+                    Name = new Translation
+                    {
+                        ContentPl = vm.ContentPl,
+                        ContentEng = vm.ContentEng
+                    }
+                };
+                _skillTypeService.Add(characteristic);
+                return RedirectToAction(nameof(Manage));
+
+            }
+            return RedirectToAction(nameof(ManageResult), vm);
+        }
+
+        [HttpDelete]
+        [ValidateAntiForgeryToken]
+        public IActionResult Remove([FromBody] string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return BadRequest();
+
+            if (_skillTypeService.GetById(Guid.Parse(id)) != null)
+            {
+                _skillTypeService.Delete(Guid.Parse(id));
+                return Ok();
+            }
+
+            return NotFound();
+        }
+
+        [HttpPatch]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromBody] TranslationBasedManageVM<SkillType> vm)
+        {
+            if (vm == null || vm.Id == Guid.Empty)
+                return BadRequest();
+
+            var characteristic = new SkillType
+            {
+                Id = vm.Id,
+                Name = new Translation
+                {
+                    ContentPl = vm.ContentPl,
+                    ContentEng = vm.ContentEng
+                }
+            };
+            _skillTypeService.Update(characteristic);
+
+            return Ok();
+        }
+    }
+}
